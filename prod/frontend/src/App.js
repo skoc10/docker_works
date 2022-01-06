758e1fbfde6832cbc343ee2fe1ca09f9cbf2a482
skoc <skoc10ds@gmail.com>
2022-01-06 21:06:12 +0300

    nodejs

--- prod/frontend/src/App.js
+++ prod/frontend/src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> selman koc</h1>
+        <h2> DevOps</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
